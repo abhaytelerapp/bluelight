654935700c9b12e39b5735e904e5703320461745
abhaytelerapp <abhay@telerapps.com>
2025-03-03 18:59:15 +0530

    update index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,14 @@
-module.exports = {
-  resolve: {
-    fallback: {
-      "path": require.resolve("path-browserify"),
-      "http": require.resolve("stream-http"),
-      "https": require.resolve("https-browserify"),
-      "crypto": require.resolve("crypto-browserify"),
-      "fs": false, // No browser equivalent for fs
-      "stream": require.resolve("stream-browserify"),
-      "zlib": require.resolve("browserify-zlib"),
-      "querystring": require.resolve("querystring-es3"),
-      "net": false, // No browser equivalent for net
-    }
-  }
+module.exports = function override(config) {
+  config.resolve.fallback = {
+    "path": require.resolve("path-browserify"),
+    "http": require.resolve("stream-http"),
+    "https": require.resolve("https-browserify"),
+    "crypto": require.resolve("crypto-browserify"),
+    "fs": false,
+    "stream": require.resolve("stream-browserify"),
+    "zlib": require.resolve("browserify-zlib"),
+    "querystring": require.resolve("querystring-es3"),
+    "net": false,
+  };
+  return config;
 };
