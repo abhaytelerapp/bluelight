5e73bbfc3f3b76e4fd999b262894520357223be8
abhaytelerapp <abhay@telerapps.com>
2025-03-03 13:18:05 +0530

    update index.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    target: "node",
+    target: "node",  // Keep this if it's for Node.js
     resolve: {
       fallback: {
         "zlib": require.resolve("browserify-zlib"),
@@ -8,10 +8,19 @@ module.exports = {
         "stream": require.resolve("stream-browserify"),
         "querystring": require.resolve("querystring-es3"),
         "http": require.resolve("stream-http"),
-        "fs": false, // fs is a Node.js module that cannot be polyfilled for the browser
-        "net": false, // Not available in browsers
-        "tls": false, // Not available in browsers
+        "https": require.resolve("https-browserify"),
+        "url": require.resolve("url/"),
+        "buffer": require.resolve("buffer/"),
+        "util": require.resolve("util/"),
+        "fs": false,  // Node.js modules that can't be polyfilled
+        "net": false, 
+        "tls": false,
       }
-    }
-  };
-  
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        process: "process/browser",
+        Buffer: ["buffer", "Buffer"],
+      }),
+    ],
+};
